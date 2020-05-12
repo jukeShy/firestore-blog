d3f2476617c9ba9945a449a2379b7edf205c53f2
jukeShy <cocax@rambler.ru>
2020-05-12 17:18:44 +0300

    initial project structure with additional webpack config aliases

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from '~components';
+import { Home } from '~pages';
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 };
