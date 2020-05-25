e125eabd9e2f721daf8dfff689255e1c8dc80977
jukeShy <cocax@rambler.ru>
2020-05-25 23:44:02 +0300

    redux devtools fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,6 @@ import { createStore, compose, applyMiddleware } from 'redux';
 import { rootReducer } from './reducers/rootReducer';
 import thunk from 'redux-thunk';
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-      window.__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
-);
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)));
 
 export { store };
